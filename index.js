12bed30579d50debe93edcdafad5e3148a4f323d
Carlo Francisco <cfrancisco726@gmail.com>
2016-10-12 23:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function getFirstSelector(selector){
   var element = document.querySelector(selector);
   return element
 }
+
+function nestedTarget(){
+  const nestT = document.querySelector('div#nested div.target');
+  return nestT
+}
+
+function increaseRankBy(n){
+  const rankList = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0, l = rankList.length; i < l; i++) {
+    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n
+    }
+    return rankList
+}
+
+function deepestChild(){
+  const deepest = document.querySelector('#grand-node div div div div');
+  return deepest
+}
